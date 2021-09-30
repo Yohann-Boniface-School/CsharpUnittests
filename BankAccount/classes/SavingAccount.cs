bccc27220cb1fecc2297664a3d11803a207a1cc5
BONIFACE <contact@yohannboniface.dev>
2021-09-30 12:00:24 +0200

    :sparkles: Adding `SavingAccount` class

---
+++ BankAccount/classes/SavingAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace BankAccount.classes
+{
+    public class SavingAccount : Account
+    {
+        private double _interestRate;
+
+        public SavingAccount(double interest, string owner, string devise) : base(owner, devise)
+        {
+            this._interestRate = interest;
+        }
+    }
+}
