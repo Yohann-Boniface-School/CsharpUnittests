860d8b03bc7015ddbd06d379ef232d427c59ac00
BONIFACE <contact@yohannboniface.dev>
2021-09-30 12:18:14 +0200

    :white_check_mark: Adding `Debit` /`Credit`

---
+++ Tests/TestDebit.cs
@@ -0,0 +1,25 @@
+﻿using BankAccount.classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestDebit
+    {
+        [TestMethod]
+        public void TestBalanceAfterDebit()
+        {
+            var account = new SavingAccount(0, "foo", "euro", 500);
+            account.Debit(100);
+            Assert.AreEqual(400, account.getBalance());
+        }
+        
+        [TestMethod]
+        public void TestBalanceAfterCredit()
+        {
+            var account = new SavingAccount(0, "foo", "euro", 500);
+            account.Credit(100);
+            Assert.AreEqual(600, account.getBalance());
+        }
+    }
+}
