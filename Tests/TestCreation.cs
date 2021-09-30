0be4c3e31ca61863e11e22596cc44d0cf39fd242
BONIFACE <contact@yohannboniface.dev>
2021-09-30 12:15:33 +0200

    :truck: Rename Unittest class

--- Tests/TestCreation.cs
+++ Tests/TestCreation.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class TestCreation
     {
         [TestMethod]
         public void TestBalanceCreation0()
