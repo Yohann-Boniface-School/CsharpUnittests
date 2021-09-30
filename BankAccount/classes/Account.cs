8323e4c3bf65c2c2870b8388a7e96165d19e89c2
BONIFACE <contact@yohannboniface.dev>
2021-09-30 12:00:16 +0200

    :sparkles: Adding `Account` class

---
+++ BankAccount/classes/Account.cs
@@ -0,0 +1,35 @@
+﻿namespace BankAccount.classes
+{
+    public class Account
+    {
+        private int _balance = 0;
+        private string _owner;
+        private string _devise;
+
+        protected Account(string owner, string devise)
+        {
+            _owner = owner;
+            _devise = devise;
+        }
+
+        public void Credit(int amount)
+        {
+            _balance += amount;
+        }
+
+        public void Debit(int amount)
+        {
+            _balance -= amount;
+        }
+
+        public int getBalance()
+        {
+            return _balance;
+        }
+
+        public int getAccountId()
+        {
+            return 0;
+        }
+    }
+}
